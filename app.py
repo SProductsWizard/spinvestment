cf90621839eb98b720a6ca0e9fb145759397f6c8
fanhongchopin <hongfan.s.f@gmail.com>
2023-03-11 19:49:56 -0500

    change app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dash
 
 # from WebApp.pages import menuPage, singleAssetReplinePage, rampAssetPage, warehousePage
 
-from WebApp.pages import singleAssetReplinePage
+from WebApp.pages import singleAssetReplinePage, rampAssetPage, warehousePage
 
 app = dash.Dash(__name__)
 server = app.server
